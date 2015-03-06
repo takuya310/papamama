17ab4d53e9f7a63bd3f9b71c1a9b3d6b254bdddc
Takuya Sato <takuya310@gmail.com>
2015-03-06 23:16:35 +0900

    Update starting point.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // 地図表示時の中心座標
-var init_center_coords = [141.347899, 43.063968];
+var init_center_coords = [135.700555, 34.692078];
 
 // Bing APIのキー
 var bing_api_key = 'AhGQykUKW2-u1PwVjLwQkSA_1rCTFESEC7bCZ0MBrnzVbVy7KBHsmLgwW_iRJg17';
